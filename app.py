0531582ab2f0ac014ccbb96d1d1aca1a57d6da35
Tinshu Sasi <thetinshusasi@gmail.com>
2019-06-09 19:41:07 +0530

    first program

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 print("hi this is tinshu")
+message = "hi i am master"
+print(message)
+message = input()
+print(message)
+print(message.title())
+firstName = "tinshu"
+lastName = "Sasi"
+fullName = firstName + " " + lastName
+print(fullName)
