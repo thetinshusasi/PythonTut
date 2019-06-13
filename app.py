b408ac852511476d53fc20b12cb345703e9712f9
Tinshu Sasi <thetinshusasi@gmail.com>
2019-06-13 07:28:26 +0530

    List learned

--- app.py
+++ app.py
@@ -1,10 +1,46 @@
-print("hi this is tinshu")
-message = "hi i am master"
-print(message)
-message = input()
-print(message)
-print(message.title())
-firstName = "tinshu"
-lastName = "Sasi"
-fullName = firstName + " " + lastName
-print(fullName)
+# print("hi this is tinshu")
+# message = "hi i am master"
+# print(message)
+# message = input()
+# print(message)
+# print(message.title())
+# firstName = "tinshu"
+# lastName = "Sasi"
+# fullName = firstName + " " + lastName
+# print(fullName)
+# name = "tinshu "
+# print(name.rstrip().__len__())
+# print(name.__len__())
+# # to remove it permanently
+# name= name.rstrip();
+# print(name.__len__())
+
+# age = 20;
+# msg = 'happy ' + str(age) + ' Birthday'
+# print(msg)
+#
+# data = 3/2
+# print(data)
+# import  this
+# msg = 'tinshu'
+# msg = msg.title()
+# print(msg)
+firstList = [1, 2, 3, 4.9, 'ko', 6]
+print(firstList)
+print(firstList[0])
+# gives you the last element in the array
+print(firstList[-1])
+firstList[2] = 100
+print(firstList)
+firstList.append(300)
+print(firstList)
+firstList.insert(1,500)
+print(firstList)
+del firstList[1]
+print(firstList)
+ele = firstList.pop()
+print(ele)
+print(firstList)
+ele1 = firstList.pop(0)
+print(ele1)
+print(firstList)
