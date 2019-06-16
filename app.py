7512e14f42a440c82ceccd2c9b01f5bb7f841ed6
Tinshu Sasi <thetinshusasi@gmail.com>
2019-06-16 13:19:03 +0530

    temp commit

--- app.py
+++ app.py
@@ -25,22 +25,79 @@
 # msg = 'tinshu'
 # msg = msg.title()
 # print(msg)
-firstList = [1, 2, 3, 4.9, 'ko', 6]
-print(firstList)
-print(firstList[0])
-# gives you the last element in the array
-print(firstList[-1])
-firstList[2] = 100
-print(firstList)
-firstList.append(300)
-print(firstList)
-firstList.insert(1,500)
-print(firstList)
-del firstList[1]
-print(firstList)
-ele = firstList.pop()
-print(ele)
-print(firstList)
-ele1 = firstList.pop(0)
-print(ele1)
-print(firstList)
+firstList = [1, 2, 3, 4.9,  6]
+# print(firstList)
+# print(firstList[0])
+# # gives you the last element in the array
+# print(firstList[-1])
+# firstList[2] = 100
+# print(firstList)
+# firstList.append(300)
+# print(firstList)
+# firstList.insert(1,500)
+# print(firstList)
+# del firstList[1]
+# print(firstList)
+# ele = firstList.pop()
+# print(ele)
+# print(firstList)
+# ele1 = firstList.pop(0)
+# print(ele1)
+# print(firstList)
+# firstList.remove('ko')
+# print(firstList)
+# firstList.sort()
+# firstList.sort(reverse=True)
+# print(firstList)
+# print(sorted(firstList))
+# print(len( firstList))
+# for item in firstList:
+#     print('here is the item : ' + str(item))
+#     print('what is the value in item : ' + str(item))
+#
+# even_number = list(range(2,11,2))
+# print(even_number)
+#
+# digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+# print(min(digits))
+# print(max(digits))
+# print(sum(digits))
+# # list comprehension
+# squares = [value**2 for value in range(1,11)]
+# print(squares)
+# players = ['charles', 'martina', 'michael', 'florence', 'eli']
+# print(players[-3:])
+#
+# my_foods = ['pizza', 'falafel', 'carrot cake']
+# friend_foods = my_foods[:]
+# print(friend_foods)
+# alien = {'color': 'green', 'points': 5}
+# print(alien)
+# x = alien['color']
+# print(x)
+# x = 'ghost'
+# print(x)
+# print(alien)
+#
+# user_0 = {
+#     'username': 'efermi',
+#     'first': 'enrico',
+#     'last': 'fermi',
+# }
+# print(user_0.items())
+# data = user_0.items()
+# print(data)
+# # for key, value in user_0.items():
+# #     print("\nKey: " + key)
+# #     print("Value: " + value)
+# name = input("Please enter your name: ")
+# print(name)
+
+# def print_name(name):
+#     # hohoho
+#     """" dead man walking
+#     :rtype: object
+#     """
+#     print(name)
+
+
